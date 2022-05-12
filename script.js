0f98d834f6b3a0ae4242385b8060e3ea9d30bf9d
tburakonat <tburakonat@gmail.com>
2022-05-12 13:38:06 +0200

    update script order

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const userSection = document.querySelector(".user-section")
 const userScoreDisplay = document.getElementById("user-score")
 const computerScoreDisplay = document.getElementById("computer-score")
+let gameInfo = {
+    winner: "",
+    userScore: 0,
+    computerScore: 0,
+    round: 0
+}
 
 userSection.addEventListener("click", function handleClick(e){
     if (e.target.id == "rock" || e.target.id == "paper" || e.target.id == "scissors") {
@@ -20,33 +26,61 @@ userSection.addEventListener("click", function handleClick(e){
         if (gameInfo.userScore === 5 || gameInfo.computerScore === 5) {
             finishGame(gameInfo)
             userSection.removeEventListener("click", handleClick)
+            // window.removeEventListener("keyup", handleKeyPress)
         }
     }
 })
 
-let winner = ""
-let userScore = 0
-let computerScore = 0
-let round = 0
-let gameInfo = {}
+/*
+window.addEventListener("keyup", function handleKeyPress(e) {
+    if (e.key == "r" || e.key == "p" || e.key == "s") {
+        let target
+        if (e.key === "r") {
+            target = "rock"
+        } else if (e.key === "p") {
+            target = "paper"
+        } else if (e.key === "s") {
+            target = "scissors"
+        }
+        let rock = document.getElementById("rock")
+        let paper = document.getElementById("paper")
+        let scissors = document.getElementById("scissors")
+        let userSelection = document.getElementById(target)
+        rock.style.backgroundColor = "#eee"
+        rock.style.transform = "scale(0.9)"
+        paper.style.backgroundColor = "#eee"
+        paper.style.transform = "scale(0.9)"
+        scissors.style.backgroundColor = "#eee"
+        scissors.style.transform = "scale(0.9)"
+        userSelection.style.backgroundColor = "#49be25"
+        userSelection.style.transform = "scale(1.1)"
+        let gameInfo = playRound(computerPlay, userSelection)
+        if (gameInfo.userScore === 5 || gameInfo.computerScore === 5) {
+            finishGame(gameInfo)
+            window.removeEventListener("keyup", handleKeyPress)
+            userSection.removeEventListener("click", handleClick)
+        }
+    }
+})
+*/
 
 function playRound(computerPlay, userPlay) {
     let computerSelection = computerPlay()
     let userSelection = userPlay.id.toLowerCase()
-    winner = determineWinner(computerSelection, userSelection)
-    round ++
-    if (winner === "user") {
-        userScore ++
-        userScoreDisplay.textContent = userScore
-    } else if (winner === "computer") {
-        computerScore ++
-        computerScoreDisplay.textContent = computerScore
+    gameInfo.winner = determineWinner(computerSelection, userSelection)
+    gameInfo.round ++
+    if (gameInfo.winner === "user") {
+        gameInfo.userScore ++
+        userScoreDisplay.textContent = gameInfo.userScore
+    } else if (gameInfo.winner === "computer") {
+        gameInfo.computerScore ++
+        computerScoreDisplay.textContent = gameInfo.computerScore
     }
     let roundInfo = {
-       winner,
-       userScore,
-       computerScore,
-       round
+        winner: gameInfo.winner,
+        userScore: gameInfo.userScore,
+        computerScore: gameInfo.computerScore,
+        round: gameInfo.round
     }
     gameInfo = roundInfo
     return gameInfo
